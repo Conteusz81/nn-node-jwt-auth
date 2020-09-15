474f43e008f14f90f911a5a7a001b975606bbaa6
Piotr Pawlica <piotr.pawlica@codeconcept.pl>
2020-09-15 15:14:17 +0200

    User log in / JWT & cookie-parser

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,15 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 const handleErrors = err => {
-    let errors = { email: '', password: ''};
+    let errors = { email: '', password: '', login: ''};
 
     if (err.code === 11000) {
         errors.email = 'This email is already registered';
+        return errors;
+    }
+
+    if (err.message === 'incorrect login') {
+        errors.login = 'Login or Password is incorrect';
         return errors;
     }
 
@@ -43,7 +48,17 @@ const signup_post = async (req, res) => {
 
 const login_post = async (req, res) => {
     const { email, password } = req.body;
-    res.send('logged in');
+
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json( { user: user._id });
+    }
+    catch (err) {
+        const errors = handleErrors(err)
+        res.status(400).json({ errors });
+    }
 };
 
 module.exports = {
