d43f0fd4b441dfba855b280781ba8b0214d66dd7
Piotr Pawlica <piotr.pawlica@codeconcept.pl>
2020-09-16 09:00:10 +0200

    User log out

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,8 @@ const {
     signup_get,
     signup_post,
     login_get,
-    login_post
+    login_post,
+    logout_get
 } = require('../controllers/authController')
 
 const router = Router();
@@ -12,5 +13,6 @@ router.get('/signup', signup_get);
 router.post('/signup', signup_post);
 router.get('/login', login_get);
 router.post('/login', login_post);
+router.get('/logout', logout_get);
 
 module.exports = router;
